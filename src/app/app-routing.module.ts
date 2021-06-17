baa52314f030c103424f2a15bc1f1d9f6411acee
BogdanBida <bogdan_bida@mail.ua>
2021-06-17 22:43:06 +0300

    Setup basic project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'home',
+        loadChildren: (): any =>
+          import('./features/home/home.module').then(
+            (module) => module.HomeModule
+          ),
+      },
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full',
+      },
+      {
+        path: '**',
+        pathMatch: 'full',
+        redirectTo: '/home',
+        // data: { state: 'notfound' }
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
