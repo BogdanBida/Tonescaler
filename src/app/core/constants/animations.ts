2de0ab3ee4bc8e7596f0b101729a00d505f27b71
BogdanBida <bogdan_bida@mail.ua>
2021-06-18 21:49:05 +0300

    Updated themes

--- src/app/core/constants/animations.ts
+++ src/app/core/constants/animations.ts
@@ -1,33 +1,24 @@
 import { animate, style, transition, trigger } from '@angular/animations';
 
+const easingFn = '300ms ease-in-out';
+const translateOffset = 33;
+
 export const routeAnimations = trigger('routeAnimation', [
   transition('center => bottom', [
-    style({ opacity: 0, transform: 'translateY(100%)' }),
-    animate(
-      '250ms ease-in-out',
-      style({ opacity: 1, transform: 'translateY(0)' })
-    ),
+    style({ opacity: 0, transform: `translateY(${translateOffset}%)` }),
+    animate(easingFn, style({ opacity: 1, transform: 'translateY(0)' })),
   ]),
   transition('bottom => center', [
-    style({ opacity: 0, transform: 'translateY(-100%)' }),
-    animate(
-      '250ms ease-in-out',
-      style({ opacity: 1, transform: 'translateY(0)' })
-    ),
+    style({ opacity: 0, transform: `translateY(-${translateOffset}%)` }),
+    animate(easingFn, style({ opacity: 1, transform: 'translateY(0)' })),
   ]),
   transition('center => left', [
-    style({ opacity: 0, transform: 'translateX(-100%)' }),
-    animate(
-      '250ms ease-in-out',
-      style({ opacity: 1, transform: 'translateX(0)' })
-    ),
+    style({ opacity: 0, transform: `translateX(-${translateOffset}%)` }),
+    animate(easingFn, style({ opacity: 1, transform: 'translateX(0)' })),
   ]),
   transition('left => center', [
-    style({ opacity: 0, transform: 'translateX(100%)' }),
-    animate(
-      '250ms ease-in-out',
-      style({ opacity: 1, transform: 'translateX(0)' })
-    ),
+    style({ opacity: 0, transform: `translateX(${translateOffset}%)` }),
+    animate(easingFn, style({ opacity: 1, transform: 'translateX(0)' })),
   ]),
   transition('* <=> *', [
     style({ opacity: 0 }),
--- src/app/features/settings/settings.module.ts
+++ src/app/features/settings/settings.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
 import { SharedModule } from './../../shared/shared.module';
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsComponent } from './settings.component';
@@ -13,6 +14,7 @@ import { ThemeSwitcherComponent } from './theme-switcher/theme-switcher.componen
     FormsModule,
     ReactiveFormsModule,
     SettingsRoutingModule,
+    TranslateModule.forChild(),
   ],
   declarations: [SettingsComponent, ThemeSwitcherComponent],
 })
