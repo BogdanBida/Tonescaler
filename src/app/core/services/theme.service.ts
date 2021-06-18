279b834e18637f229434a147bbacb19443e343b3
BogdanBida <bogdan_bida@mail.ua>
2021-06-18 23:04:39 +0300

    Added theme saving

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Theme } from './../enums/themes.enum';
+import { AppCookieService } from './app-cookie.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ThemeService {
+  constructor(private readonly _cookieService: AppCookieService) {}
+
   public selectedTheme: Theme | null = null;
 
   public setTheme(themeName: Theme): void {
     this.selectedTheme = themeName;
+    this._cookieService.saveSelectedTheme(themeName);
     document.documentElement.setAttribute('data-theme', themeName);
   }
 }
