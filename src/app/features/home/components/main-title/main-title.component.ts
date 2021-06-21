1a2f91d67dd969bf6243318822e0b3cb49c5d9ca
BogdanBida <bogdan_bida@mail.ua>
2021-06-21 14:56:40 +0300

    Moved main title to component

---
+++ src/app/features/home/components/main-title/main-title.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-title',
+  templateUrl: './main-title.component.html',
+  styleUrls: ['./main-title.component.scss']
+})
+export class MainTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { CircularMenuComponent } from './components/circular-menu/circular-menu.component';
 import { HelperMenuComponent } from './components/helper-menu/helper-menu.component';
+import { MainTitleComponent } from './components/main-title/main-title.component';
 import { MusicboxButtonComponent } from './components/musicbox-button/musicbox-button.component';
 import { SettingsButtonComponent } from './components/settings-button/settings-button.component';
 import { HomeRoutingModule } from './home-routing.module';
@@ -16,6 +17,7 @@ import { HomeComponent } from './home.component';
     CircularMenuComponent,
     MusicboxButtonComponent,
     HelperMenuComponent,
+    MainTitleComponent,
   ],
 })
 export class HomeModule {}
