e3971b820a3ac26829dc6b01d6636ff0b26bb5f0
BogdanBida <bogdan_bida@mail.ua>
2021-06-21 16:47:06 +0300

    Added convertors

---
+++ src/app/core/constants/musictheory.ts
@@ -0,0 +1,3 @@
+export const FQ_STANDART_A4 = 440;
+export const FQ_OCTAVE = 1200;
+export const FOURTH_OCTAVE = 69;
--- src/app/core/utils/convertors.ts
+++ src/app/core/utils/convertors.ts
@@ -1,10 +1,15 @@
 import { NOTES, OCTAVE_OFFSET } from '../constants/notes';
+import {
+  FOURTH_OCTAVE,
+  FQ_OCTAVE,
+  FQ_STANDART_A4,
+} from './../constants/musictheory';
 
-export function NoteNumToString(note: number): string {
+export function NoteToString(note: number): string {
   return NOTES[note % 12] + Math.trunc((note - OCTAVE_OFFSET) / 12) + 1;
 }
 
-export function StringToNoteNum(note: string): number {
+export function StringToNote(note: string): number {
   // TODO: try regex to validate note param
   const noteBase = note.slice(0, note.length - 1);
 
@@ -21,4 +26,26 @@ export function StringToNoteNum(note: string): number {
   }
 
   return noteIndex + octave * 12 + 1;
+}
+
+export function nearestNoteByFrequency(
+  freq: number,
+  base: number = FQ_STANDART_A4
+): number {
+  const noteNum = 12 * (Math.log(freq / base) / Math.log(2));
+
+  return Math.round(noteNum) + FOURTH_OCTAVE;
+}
+
+export function frequencyFromNote(
+  note: number,
+  base: number = FQ_STANDART_A4
+): number {
+  return base * Math.pow(2, (note - FOURTH_OCTAVE) / 12);
+}
+
+export function centsOffFromPitch(frequency: number, note: number): number {
+  return Math.floor(
+    (FQ_OCTAVE * Math.log(frequency / frequencyFromNote(note))) / Math.log(2)
+  );
 }
