cdf7d3808b0fc5764b1b1a3a4b51e879bebe2bd0
BogdanBida <bogdan_bida@mail.ua>
2021-06-21 17:07:12 +0300

    Updated import

--- src/app/core/services/audio-player.service.ts
+++ src/app/core/services/audio-player.service.ts
@@ -5,7 +5,7 @@ import {
   Player,
 } from 'soundfont-player';
 import { InstrumentCollection } from '../models/instrument-collection';
-import { NoteNumToString } from '../utils/convertors';
+import { NoteToString } from '../utils/convertors';
 
 @Injectable({
   providedIn: 'root',
@@ -36,7 +36,7 @@ export class AudioPlayerService {
       this._instruments[instrumentName] ||
       (await this.initInstrument(instrumentName));
 
-    const noteName = NoteNumToString(note);
+    const noteName = NoteToString(note);
 
     instrument.play(noteName);
   }
