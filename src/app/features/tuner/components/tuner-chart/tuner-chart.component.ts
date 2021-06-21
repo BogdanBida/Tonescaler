3fc7be14aee87ac4e4ac53a16f654d64e0d152c0
BogdanBida <bogdan_bida@mail.ua>
2021-06-21 17:47:18 +0300

    Added canvas to tuner chart

--- src/app/features/tuner/components/tuner-chart/tuner-chart.component.ts
+++ src/app/features/tuner/components/tuner-chart/tuner-chart.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-tuner-chart',
   templateUrl: './tuner-chart.component.html',
-  styleUrls: ['./tuner-chart.component.scss']
+  styleUrls: ['./tuner-chart.component.scss'],
 })
-export class TunerChartComponent implements OnInit {
+export class TunerChartComponent implements AfterViewInit {
+  constructor(private readonly _self: ElementRef) {}
 
-  constructor() { }
+  @ViewChild('canv') public canv!: ElementRef;
 
-  ngOnInit() {
+  @HostListener('window:resize', ['$event'])
+  public onResize(event: Event): void {
+    const { clientHeight, clientWidth } = this._self.nativeElement;
+
+    if (this.canv) {
+      this.canv.nativeElement.width = clientWidth;
+      this.canv.nativeElement.height = clientHeight;
+    }
   }
 
+  public ngAfterViewInit(): void {}
 }
