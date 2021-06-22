a8df72eff62295ace10f47f9f88149a4b11d68c6
BogdanBida <bogdan_bida@mail.ua>
2021-06-23 02:05:50 +0300

    Changed window resize event to host resize event

--- src/app/features/tuner/components/tuner-chart/tuner-chart.component.ts
+++ src/app/features/tuner/components/tuner-chart/tuner-chart.component.ts
@@ -2,9 +2,14 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
-  HostListener,
+  Inject,
   ViewChild,
 } from '@angular/core';
+import {
+  ResizeObserverService,
+  RESIZE_OPTION_BOX,
+} from '@ng-web-apis/resize-observer';
+import { FQ_STANDART_A4 } from 'src/app/core/constants';
 import { TunerService } from 'src/app/core/services';
 
 const NOTE_SIZE = 8;
@@ -15,12 +20,25 @@ const BOTTOM_OFFSET = 32;
   selector: 'app-tuner-chart',
   templateUrl: './tuner-chart.component.html',
   styleUrls: ['./tuner-chart.component.scss'],
+  providers: [
+    ResizeObserverService,
+    {
+      provide: RESIZE_OPTION_BOX,
+      useValue: 'border-box',
+    },
+  ],
 })
 export class TunerChartComponent implements AfterViewInit {
   constructor(
     private readonly _self: ElementRef,
+    @Inject(ResizeObserverService)
+    private readonly _entries$: ResizeObserverService,
     private readonly _tunerService: TunerService
-  ) {}
+  ) {
+    this._entries$.subscribe((entries) => {
+      this.onResize();
+    });
+  }
 
   @ViewChild('canv') public canv!: ElementRef;
 
@@ -36,7 +54,6 @@ export class TunerChartComponent implements AfterViewInit {
 
   private readonly _lastValues: number[] = [];
 
-  @HostListener('window:resize')
   public onResize(): void {
     const { clientHeight, clientWidth } = this._self.nativeElement;
 
@@ -55,31 +72,31 @@ export class TunerChartComponent implements AfterViewInit {
     this._ctx = this.canv.nativeElement.getContext('2d');
     this.onResize();
 
-    // this._tunerService.currentFrequency.subscribe((freq) => {
-    //   if (!this._canvWidth && !this._canvHeight) {
-    //     return;
-    //   }
+    this._tunerService.currentFrequency$.subscribe((freq) => {
+      if (!this._canvWidth && !this._canvHeight) {
+        return;
+      }
 
-    //   this.frequency = freq as number;
-    //   console.log(freq);
+      this.frequency = freq as number;
+      console.log(freq);
 
-    //   // todo: group
-    //   this._lastValues.unshift(freq as number);
-    //   this._lastValues.length > this._cacheSize && this._lastValues.pop();
+      // todo: group
+      this._lastValues.unshift(freq as number);
+      this._lastValues.length > this._cacheSize && this._lastValues.pop();
 
-    //   const MAX_FQ = 522;
+      const MAX_FQ = 522;
 
-    //   this._ctx.clearRect(0, 0, this._canvWidth, this._canvHeight);
-    //   this._lastValues.forEach((value, i) => {
-    //     this._ctx?.fillRect(
-    //       ((Number(value) - FQ_STANDART_A4) / MAX_FQ) * this._canvWidth,
-    //       this._canvHeight -
-    //         BOTTOM_OFFSET -
-    //         (i / this._cacheSize) * (this._canvHeight - BOTTOM_OFFSET),
-    //       NOTE_SIZE,
-    //       NOTE_SIZE
-    //     );
-    //   });
-    // });
+      this._ctx.clearRect(0, 0, this._canvWidth, this._canvHeight);
+      this._lastValues.forEach((value, i) => {
+        this._ctx?.fillRect(
+          ((Number(value) - FQ_STANDART_A4) / MAX_FQ) * this._canvWidth,
+          this._canvHeight -
+            BOTTOM_OFFSET -
+            (i / this._cacheSize) * (this._canvHeight - BOTTOM_OFFSET),
+          NOTE_SIZE,
+          NOTE_SIZE
+        );
+      });
+    });
   }
 }
