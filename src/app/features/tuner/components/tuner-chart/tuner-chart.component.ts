9fc3762391c6b6c285492c71f417e9d51386f13c
BogdanBida <bogdan_bida@mail.ua>
2021-06-22 23:45:41 +0300

    Added test drawing on tuner chart

--- src/app/features/tuner/components/tuner-chart/tuner-chart.component.ts
+++ src/app/features/tuner/components/tuner-chart/tuner-chart.component.ts
@@ -5,6 +5,12 @@ import {
   HostListener,
   ViewChild,
 } from '@angular/core';
+import { FQ_STANDART_A4 } from './../../../../core/constants/musictheory';
+import { TunerService } from './../../../../core/services/tuner.service';
+
+const NOTE_SIZE = 8;
+
+const BOTTOM_OFFSET = 32;
 
 @Component({
   selector: 'app-tuner-chart',
@@ -12,13 +18,33 @@ import {
   styleUrls: ['./tuner-chart.component.scss'],
 })
 export class TunerChartComponent implements AfterViewInit {
-  constructor(private readonly _self: ElementRef) {}
+  constructor(
+    private readonly _self: ElementRef,
+    private readonly _tunerService: TunerService
+  ) {}
 
   @ViewChild('canv') public canv!: ElementRef;
 
-  @HostListener('window:resize', ['$event'])
-  public onResize(event: Event): void {
+  public frequency = 0;
+
+  private _canvWidth!: number;
+
+  private _canvHeight!: number;
+
+  private _ctx!: CanvasRenderingContext2D;
+
+  private _cacheSize = 0;
+
+  private readonly _lastValues: number[] = [];
+
+  @HostListener('window:resize')
+  public onResize(): void {
     const { clientHeight, clientWidth } = this._self.nativeElement;
+
+    this._canvHeight = clientHeight;
+    this._canvWidth = clientWidth;
+
+    this._cacheSize = this._canvHeight / NOTE_SIZE;
 
     if (this.canv) {
       this.canv.nativeElement.width = clientWidth;
@@ -26,5 +52,35 @@ export class TunerChartComponent implements AfterViewInit {
     }
   }
 
-  public ngAfterViewInit(): void {}
+  public ngAfterViewInit(): void {
+    this._ctx = this.canv.nativeElement.getContext('2d');
+    this.onResize();
+
+    this._tunerService.currentFrequency.subscribe((freq) => {
+      if (!this._canvWidth && !this._canvHeight) {
+        return;
+      }
+
+      this.frequency = freq as number;
+      console.log(freq);
+
+      // todo: group
+      this._lastValues.unshift(freq as number);
+      this._lastValues.length > this._cacheSize && this._lastValues.pop();
+
+      const MAX_FQ = 522;
+
+      this._ctx.clearRect(0, 0, this._canvWidth, this._canvHeight);
+      this._lastValues.forEach((value, i) => {
+        this._ctx?.fillRect(
+          ((Number(value) - FQ_STANDART_A4) / MAX_FQ) * this._canvWidth,
+          this._canvHeight -
+            BOTTOM_OFFSET -
+            (i / this._cacheSize) * (this._canvHeight - BOTTOM_OFFSET),
+          NOTE_SIZE,
+          NOTE_SIZE
+        );
+      });
+    });
+  }
 }
