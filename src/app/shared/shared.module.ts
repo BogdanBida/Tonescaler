0b172cf3b46e4df80ce87f20f06328e2ca6e81dc
BogdanBida <bogdan_bida@mail.ua>
2021-06-23 01:27:41 +0300

    Added pipe to module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,8 +3,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { GotohomeComponent } from './components/gotohome/gotohome.component';
 import { MininavComponent } from './components/mininav/mininav.component';
+import { NotenamePipe } from './pipes/notename.pipe';
 
-const exports = [GotohomeComponent, MininavComponent];
+const exports = [GotohomeComponent, MininavComponent, NotenamePipe];
 
 @NgModule({
   imports: [CommonModule, RouterModule],
