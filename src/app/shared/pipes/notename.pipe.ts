ae03f24a76f538609f9da1aaea102492ce5b5258
BogdanBida <bogdan_bida@mail.ua>
2021-06-23 01:27:30 +0300

    Fixed method name

--- src/app/shared/pipes/notename.pipe.ts
+++ src/app/shared/pipes/notename.pipe.ts
@@ -1,11 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { NoteNumToString } from 'src/app/core/utils/convertors';
+import { noteToString } from 'src/app/core/utils/convertors';
 
 @Pipe({
   name: 'notename',
 })
 export class NotenamePipe implements PipeTransform {
   public transform(value: number, args?: any): any {
-    return NoteNumToString(value);
+    return noteToString(value);
   }
 }
