1f2a52f68dba5445305f0ae0630923e462f49b2f
BogdanBida <bogdan_bida@mail.ua>
2021-06-25 23:17:38 +0300

    Updated 404 page

--- src/app/features/not-found/not-found.component.ts
+++ src/app/features/not-found/not-found.component.ts
@@ -1,8 +1,40 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import { AudioPlayerService } from './../../core/services/audio-player.service';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss'],
 })
-export class NotFoundComponent {}
+export class NotFoundComponent implements AfterViewInit {
+  constructor(
+    private readonly _self: ElementRef,
+    private readonly _audioPlayerService: AudioPlayerService
+  ) {}
+
+  public freenotes: any[] = [];
+
+  public playNote(index: number): void {
+    this._audioPlayerService.play('alto_sax', this.freenotes[index].note);
+  }
+
+  public ngAfterViewInit(): void {
+    this._audioPlayerService.initInstrument('alto_sax');
+    const { clientWidth, clientHeight } = this._self.nativeElement;
+
+    const amount = 20;
+
+    // TODO: Brownian motion
+    // range(amount).forEach((_) => {
+    //   this.freenotes.push({
+    //     note: 12 + Math.round(Math.random() * amount),
+    //     style: {
+    //       top: `${Math.random() * clientHeight - 44}px`,
+    //       left: `${Math.random() * clientWidth - 44}px`,
+    //       'animation-duration': 10 + Math.random() * 20 + 's',
+    //     },
+    //     initialDegree: Math.random() * 360,
+    //   });
+    // });
+  }
+}
--- src/app/features/not-found/not-found.module.ts
+++ src/app/features/not-found/not-found.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { NotFoundRoutingModule } from './not-found-page-routing.module';
 import { NotFoundComponent } from './not-found.component';
 
 @NgModule({
-  imports: [CommonModule, NotFoundRoutingModule],
+  imports: [CommonModule, NotFoundRoutingModule, TranslateModule.forChild()],
   declarations: [NotFoundComponent],
 })
 export class NotFoundModule {}
