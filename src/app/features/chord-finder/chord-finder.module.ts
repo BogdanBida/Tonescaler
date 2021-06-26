b0bc614a9539ee52e04bfb22d1467e2e2b0d4ff0
BogdanBida <bogdan_bida@mail.ua>
2021-06-26 10:01:31 +0300

    Updated chord-finder

--- src/app/features/chord-finder/chord-finder.module.ts
+++ src/app/features/chord-finder/chord-finder.module.ts
@@ -2,17 +2,20 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { ChordFinderRoutingModule } from './chord-finder-routing.module';
 import { ChordFinderComponent } from './chord-finder.component';
+import { InfoComponent } from './components/info/info.component';
 import { PianoKeyboardComponent } from './components/piano-keyboard/piano-keyboard.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
     ChordFinderRoutingModule,
     ReactiveFormsModule,
     TranslateModule.forChild(),
   ],
-  declarations: [ChordFinderComponent, PianoKeyboardComponent],
+  declarations: [ChordFinderComponent, PianoKeyboardComponent, InfoComponent],
 })
 export class ChordFinderModule {}
