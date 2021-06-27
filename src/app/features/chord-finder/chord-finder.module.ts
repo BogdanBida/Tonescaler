7826bb5073fe7e3e3f59e9578be66e9019377d47
BogdanBida <bogdan_bida@mail.ua>
2021-06-27 13:11:41 +0300

    Updated design

--- src/app/features/chord-finder/chord-finder.module.ts
+++ src/app/features/chord-finder/chord-finder.module.ts
@@ -1,21 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { TranslateModule } from '@ngx-translate/core';
-import { SharedModule } from 'src/app/shared/shared.module';
 import { ChordFinderRoutingModule } from './chord-finder-routing.module';
 import { ChordFinderComponent } from './chord-finder.component';
+import { ControlsComponent } from './components/controls/controls.component';
 import { InfoComponent } from './components/info/info.component';
 import { PianoKeyboardComponent } from './components/piano-keyboard/piano-keyboard.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    SharedModule,
     ChordFinderRoutingModule,
     ReactiveFormsModule,
+    MatSelectModule,
+    MatSlideToggleModule,
     TranslateModule.forChild(),
   ],
-  declarations: [ChordFinderComponent, PianoKeyboardComponent, InfoComponent],
+  declarations: [
+    ChordFinderComponent,
+    PianoKeyboardComponent,
+    InfoComponent,
+    ControlsComponent,
+  ],
 })
 export class ChordFinderModule {}
